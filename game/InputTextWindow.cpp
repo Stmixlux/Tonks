368f6b962e996dd13d660451d9caa0da6786aea2
Timur Nasybullin <124610144+Stmixlux@users.noreply.github.com>
2024-05-03 19:20:35 +0300

    Squashed commit of the following:
    commit c06d23f3560805801cb6cc3ac1155611f2a617ca
    Author: Артемий Гаврилов <artyomgavr04@gmail.com>
    Date:   Sat Apr 27 14:03:37 2024 +0300
    
        Finished IP-getting-box

--- game/InputTextWindow.cpp
+++ game/InputTextWindow.cpp
@@ -11,3 +11,67 @@ void InputTextWindow::DrawInputTextWindow()
 	DrawText(text, Position.x - MeasureText(text, FontSize) / 2, Position.y - FontSize / 2, FontSize, BLACK);
 	DrawText(Description, Position.x - MeasureText(Description, DescriptionFontSize) - Size.x / 2 - 10, Position.y - DescriptionFontSize / 2, DescriptionFontSize, BLACK);
 }
+
+void InputTextWindow::UpdateState()
+{
+	Vector2 MousePos = GetMousePosition();
+	if (CheckCollisionPointRec(MousePos, Rectangle{ Position.x - Size.x / 2, Position.y - Size.y / 2, Size.x, Size.y }) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+		IsActive = true;
+		SetMouseCursor(MOUSE_CURSOR_IBEAM);
+	}
+	else if (IsKeyPressed(KEY_ENTER) || (!CheckCollisionPointRec(MousePos, Rectangle{ Position.x - Size.x / 2, Position.y - Size.y / 2, Size.x, Size.y }) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))) {
+		IsActive = false;
+		SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+	}
+}
+
+std::string InputTextWindow::GetIp()
+{
+	if (TextLen != 0) {
+		std::string IP_port{ text };
+		if (IP_port.find(':') < 30) {
+			return IP_port.substr(0, IP_port.find(':'));
+		}
+	}
+	return std::string("0");
+}
+
+int InputTextWindow::GetPort()
+{
+	if (TextLen != 0) {
+		std::string IP_port{ text };
+		if (IP_port.find(':') < 30 && IP_port[IP_port.find(':') + 1] != '\0') {
+			return stoi(IP_port.substr(IP_port.find(':') + 1, IP_port.find('\0')));
+		}
+	}
+	return 0;
+	
+}
+
+
+void InputTextWindow::UpdateText()
+{
+	if (IsActive) {
+		int key = GetKeyPressed();
+		if (key > 0 && TextLen < 23) {
+			if (IsKeyDown(KEY_LEFT_SHIFT) && IsKeyPressed(KEY_SEMICOLON)) {
+				text[TextLen] = ':';
+				text[TextLen + 1] = '\0';
+				TextLen++;
+			}
+			else if (key == 46 || (key >= 48 && key <= 57)) {
+				text[TextLen] = (char)key;
+				text[TextLen + 1] = '\0';
+				TextLen++;
+			}
+			int key = GetKeyPressed();
+		}
+		if (IsKeyPressed(KEY_BACKSPACE))
+		{
+			TextLen--;
+			if (TextLen < 0) TextLen = 0;
+			text[TextLen] = '\0';
+		}
+	}
+}
+
--- game/InputTextWindow.h
+++ game/InputTextWindow.h
@@ -6,17 +6,21 @@ class InputTextWindow
 public:
 	Vector2 Position;		// Position of the center of the button
 	Vector2 Size;
-	const char* text = "\0";
+	char text[30]{' '};
 	const char* Description;
 
-	int FontSize = 14;
+	int FontSize = 30;
 	int DescriptionFontSize;
+	bool IsActive = false;
+	int TextLen = 0;
 
 	InputTextWindow(const Vector2& _p, const Vector2& _d, const char* _desc, int _fs) : Position(_p), Size(_d), Description(_desc), DescriptionFontSize(_fs){};
 
 	void DrawInputTextWindow();
+	void UpdateState();
 	void UpdateText();
 
-
+	std::string GetIp();
+	int GetPort();
 };
 
